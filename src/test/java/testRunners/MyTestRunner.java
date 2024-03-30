38024dd2b3f7e6d760e5414db5cedce6f3ce808b
LENOVO <althafhussain.pcet@gmail.com>
2024-03-30 17:49:05 +0530

    this is my first commit

--- src/test/java/testRunners/MyTestRunner.java
+++ src/test/java/testRunners/MyTestRunner.java
@@ -3,7 +3,7 @@ package testRunners;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
+//this is a runner class
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		features = {"src/test/resources/parallelUsingTestNG/LoginPage.feature"},
